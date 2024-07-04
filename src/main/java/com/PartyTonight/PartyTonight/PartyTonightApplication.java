6ca58d90a8b72459bdff7a9f5747e41b249748ea
StoneCAU <seokwoo1999@gmail.com>
2024-07-04 15:33:19 +0900

    Initial commit

---
+++ src/main/java/com/PartyTonight/PartyTonight/PartyTonightApplication.java
@@ -0,0 +1,13 @@
+package com.PartyTonight.PartyTonight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartyTonightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartyTonightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PartyTonight/PartyTonight/PartyTonightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PartyTonight.PartyTonight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartyTonightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
